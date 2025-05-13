ee1f49cf976bde4ca16b1c9261196d2291f4fa97
Moura196 <gmendesmoura@gmail.com>
2025-05-13 20:08:30 -0300

    feat: Implementa login através de usuário criados em memória.

--- src/main/java/com/springFramework/aprender/WebSecurityConfig.java
+++ src/main/java/com/springFramework/aprender/WebSecurityConfig.java
@@ -1,25 +1,34 @@
 package com.springFramework.aprender;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig {
 	
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-			.withUser("user")
-			.password("user123")
-			.roles("USERS")
-			.and()
-			.withUser("admin")
-			.password("admin123")
-			.roles("MANAGERS");
+	@Bean
+	public UserDetailsService users() {
+		UserDetails user = User.builder()
+			.username("user")
+			.password("{noop}teste")
+			.roles("USER")
+			.build();
+		UserDetails admin = User.builder()
+			.username("admin")
+			.password("{noop}teste12")
+			.roles("USER", "ADMIN")
+			.build();
+		return new InMemoryUserDetailsManager(user, admin);
 	}
 
 }
