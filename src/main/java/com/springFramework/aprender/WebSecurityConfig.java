b60cb3e95ac56a2dc619767b8e68d0b9efad25ee
Moura196 <gmendesmoura@gmail.com>
2025-05-14 20:14:21 -0300

    feat: Cria o filtro para acesso de rota para roles específicas.

--- src/main/java/com/springFramework/aprender/WebSecurityConfig.java
+++ src/main/java/com/springFramework/aprender/WebSecurityConfig.java
@@ -2,20 +2,35 @@ package com.springFramework.aprender;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig {
 	
+	
+	protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.authorizeHttpRequests((authorize) -> authorize
+					.requestMatchers("/").permitAll()
+					.requestMatchers("/login").permitAll()
+					.requestMatchers("/users").hasAnyRole("USERS", "ADMINS")
+					.requestMatchers("/admins").hasAnyRole("ADMINS")
+					.anyRequest().authenticated()
+			);
+			
+		return http.build();
+	}
+		
 	@Bean
 	public UserDetailsService users() {
 		UserDetails user = User.builder()
@@ -26,7 +41,7 @@ public class WebSecurityConfig {
 		UserDetails admin = User.builder()
 			.username("admin")
 			.password("{noop}teste12")
-			.roles("USERS", "MANAGERS")
+			.roles("ADMINS")
 			.build();
 		return new InMemoryUserDetailsManager(user, admin);
 	}
